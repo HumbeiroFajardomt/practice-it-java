1d99189f6f572875b254d6f17dc46d2310e9f806
HumbeiroFajardomt <113655610+HumbeiroFajardomt@users.noreply.github.com>
2022-12-27 14:18:13 +0000

    Training exercises

--- _02_02/Ticket.java
+++ _02_02/Ticket.java
@@ -3,7 +3,7 @@ package _02_02;
 public class Ticket {
   public Ticket(){
 
-  }
+  } 
   private String destination;
   private double price;
   private boolean isReturn;
--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,15 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
-    // Write a for loop that prints out the numbers 1 to 10
-
-    // Write a for loop that prints out the numbers 10 to 1
     
+    // Write a for loop that prints out the numbers 1 to 10
+    for (int i = 1; i <= 10; i++) {
+      System.out.println("i Values = " + (i) );
+    }
+    // Write a for loop that prints out the numbers 10 to 1
+    for (int i = 10; i < 0; i--) {
+      System.out.println("i Values = " + (i));
+    }
 
   }
 
